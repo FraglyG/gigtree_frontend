d7138c1858120422cfdf68b2f26be82f12f60cfa
Hendrik Strydom <hendrik@isdev.co>
2025-03-21 14:01:35 +0200

    Fixed up logo sizing

--- src/components/brand/logo.vue
+++ src/components/brand/logo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="logo_container tw-flex tw-flex-row">
-        <img src="/logo.svg" class="tw-aspect-square tw-w-auto tw-max-h-16" />
+        <img src="/logo.svg" class="tw-aspect-square tw-w-auto tw-max-h-14" />
         <p class="tw-max-h-16 tw-text-[2.5rem] tw-font-bold tw-ml-2">GigTree</p>
     </div>
 </template>
