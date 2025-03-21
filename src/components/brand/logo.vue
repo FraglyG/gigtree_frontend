9073979e4f5a6c9091330ba7ab39604c990c2bc0
Hendrik Strydom <hendrik@isdev.co>
2025-03-21 09:13:47 +0200

    Logo component created

---
+++ src/components/brand/logo.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="logo_container tw-flex tw-flex-row">
+        <img src="/logo.svg" class="tw-aspect-square tw-w-auto tw-max-h-16" />
+        <p class="tw-max-h-16 tw-text-[2.5rem] tw-font-bold tw-ml-2">GigTree</p>
+    </div>
+</template>
