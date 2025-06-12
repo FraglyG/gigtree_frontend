b188df73322201b5af1ca7327a220fbca751b77c
Hendrik Strydom <hendrik@isdev.co>
2025-06-12 19:30:54 +0200

    Delete src/components/main/messages directory

--- src/components/main/messages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Message } from './Message.vue';
-export { default as ChannelList } from './ChannelList.vue';
-export { default as ChatWindow } from './ChatWindow.vue';
-export { default as InboxMain } from './InboxMain.vue';
